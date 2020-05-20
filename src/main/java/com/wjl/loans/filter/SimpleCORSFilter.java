a0c73d26d91c821194d8983f070ddb862036d071
feiniu5566 <1726005450@qq.com>
2020-05-21 00:22:23 +0800

    resolve kua yu

---
+++ src/main/java/com/wjl/loans/filter/SimpleCORSFilter.java
@@ -0,0 +1,35 @@
+package com.wjl.loans.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+// see https://www.cnblogs.com/diandianquanquan/p/10607102.html
+public class SimpleCORSFilter implements Filter {
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD");
+
+        response.setHeader("Access-Control-Max-Age", "3600");
+
+        response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, authority, content-type, version-info, X-Requested-With");
+
+
+
+        chain.doFilter(req, res);
+
+    }
+
+    public void init(FilterConfig filterConfig) {}
+
+    public void destroy() {}
+
+}
